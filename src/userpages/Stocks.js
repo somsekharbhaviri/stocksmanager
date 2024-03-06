39595c11ce3b26c7f4ffc723a672308b618ac798
MOHAMMAD ADNAN SHARIFF <adnanshariffpc@gmail.com>
2024-03-06 11:47:55 +0530

    modified:   src/style.css 	modified:   src/userpages/Stocks.js

--- src/userpages/Stocks.js
+++ src/userpages/Stocks.js
@@ -1,133 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import React, { useState, useEffect } from 'react' 
-import Stocksdata from './data/stocks.json' 
-import TrendingStocks from './data/trendingstocks.json'
-import axios from 'axios'
+export default function Stocks() {
+    const [data, setData] = useState({ top_gainers: [], stocks_in_loss: [], most_bought: [] });
+    const [trendingStocksData, setTrendingStocksData] = useState([]);
 
-
-
- 
-export default function Stocks() { 
-
-    const [data,setData] = useState({top_gainers:[]}) // empty array 
-    const [TrendingStocksdata,SetTrendingStocksdata]=useState([])
     useEffect(() => {
-        setData(Stocksdata)
-        SetTrendingStocksdata(TrendingStocks)
-        fetchdata();
+        async function fetchData() {
+            try {
+                const response = await axios.get("https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=YWK4OX6UV23LZ8IB");
+                console.log(response.data);
+                setData({
+                    top_gainers: response.data.top_gainers || [],
+                    stocks_in_loss: response.data.top_losers || [],
+                    most_bought: response.data.most_actively_traded || []
+                });
+            } catch (err) {
+                console.log(err.message);
+            }
+        }
+
+        fetchData();
     }, []);
 
-    const fetchdata=async ()=>{
-        await axios.get("https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=demo")
-        .then((response) => {
-            console.log(response.data)
-            setData(response)
-            
-        }).catch((err) => {
-            console.log(err.message)
-        });
-    }
-    
-    
-  return ( 
-    
-    <div> 
-    {
-        document.title="Stocks"
-    }
-        <div class="main-div"> 
-        
-       <div> 
-           <h2 >Top Stocks</h2> 
-           <div align="center" class="outsideboxes"> 
-                 
-           {data.map((stock, index) => ( 
-                         
-                <div key={index} className="insideboxes"> 
-                <div className='pintopstocks'> 
-                    &nbsp;&nbsp; {stock.ticker} 
-                </div> 
-                <div className="button-container"> 
-                    <button className="BuySmallButton">Buy</button> 
-                    <button className="SellSmallButton">Sell</button> 
-                </div> 
-                </div> 
-            ))} 
-            </div> 
-                       
- 
-           <br/> 
- 
-           <h2 >Stocks In Loss</h2> 
-           <div align="center" class="outsideboxes" > 
-           { 
-                data.map((Stocksdata, index)  => ( 
-                <div key={index} class="insideboxes">  
-                  
-                 <div className='pintopstocks'>
-                 &nbsp;&nbsp; 
-                 {Stocksdata.sname} 
-                 </div> 
-                  
-                 <div className="button-container"> 
-                        <button className="BuySmallButton">Buy</button> 
-                        <button className="SellSmallButton">Sell</button> 
-                    </div>  
-                 </div> 
-            )) 
-        } 
-           </div> 
- 
- 
-           <br/> 
- 
-           <h2 >Most Bought on our Website</h2> 
-           <div align="center" class="outsideboxes"> 
-           { 
-                data.map((Stocksdata, index)  => ( 
-                <div key={index} class="insideboxes">  
-                  
-                 <div className='pintopstocks'>
-                 &nbsp;&nbsp; 
-                 {Stocksdata.sname} 
-                 </div> 
-                  
-                 <div className="button-container"> 
-                        <button className="BuySmallButton">Buy</button> 
-                        <button className="SellSmallButton">Sell</button> 
-                    </div>  
-                 </div> 
-            )) 
-        } 
-           </div> 
-       </div> 
-       <div class="sub-main-div"> 
-       <div class="verticalbox"> 
-           <h2 >Trending</h2> 
-           <div align="center" class="outsideboxes" >  
-           { 
-                TrendingStocksdata.map((trendingstock, index)  => ( 
-                <div key={index} class="insideboxes" >  
-                  
-                 <div className='pintopstocks'>
-                 
-                 &nbsp;&nbsp; {trendingstock.symbol} 
-                 &nbsp;&nbsp; {trendingstock.sname} 
-                 &nbsp;&nbsp; {trendingstock.current_price}
-                 
-                 </div> 
-                  
-                 <div className="button-container"> 
-                        <button className="BuySmallButton">Buy</button> 
-                        <button className="SellSmallButton">Sell</button> 
-                    </div>  
-                 </div> 
-            )) 
-        }
-           </div> 
-       </div> 
-       </div> 
-   </div> 
-    </div> 
-  ) 
+    useEffect(() => {
+        // Fetch trending stocks data
+        // Example: const response = await axios.get("TRENDING_STOCKS_API_ENDPOINT");
+        // setTrendingStocksData(response.data);
+    }, []);
+
+    return (
+        <div>
+            {document.title = "Stocks"}
+            <div className="main-div">
+                <div>
+                    <h2>Top Stocks</h2>
+                    <div align="center" className="outsideboxes">
+                        {data.top_gainers.map((stock, index) => (
+                            <div key={index} className="insideboxes">
+                                <div className='pintopstocks'>
+                                    &nbsp;&nbsp; {stock.ticker}
+                                </div>
+                                <div className="button-container">
+                                    <button className="BuySmallButton">Buy</button>
+                                    <button className="SellSmallButton">Sell</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <br />
+                    <h2>Stocks In Loss</h2>
+                    <div align="center" className="outsideboxes">
+                        {data.stocks_in_loss.map((stock, index) => (
+                            <div key={index} className="insideboxes">
+                                <div className='pintopstocks'>
+                                    &nbsp;&nbsp; {stock.ticker}
+                                </div>
+                                <div className="button-container">
+                                    <button className="BuySmallButton">Buy</button>
+                                    <button className="SellSmallButton">Sell</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <br />
+                    <h2>Most Bought on our Website</h2>
+                    <div align="center" className="outsideboxes">
+                        {data.most_bought.map((stock, index) => (
+                            <div key={index} className="insideboxes">
+                                <div className='pintopstocks'>
+                                    &nbsp;&nbsp; {stock.ticker}
+                                </div>
+                                <div className="button-container">
+                                    <button className="BuySmallButton">Buy</button>
+                                    <button className="SellSmallButton">Sell</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="sub-main-div">
+                    <div className="verticalbox">
+                        <h2>Trending</h2>
+                        <div align="center" className="outsideboxes">
+                            {trendingStocksData.map((trendingStock, index) => (
+                                <div key={index} className="insideboxes">
+                                    <div className='pintopstocks'>
+                                        &nbsp;&nbsp; {trendingStock.ticker}
+                                    </div>
+                                    <div className="button-container">
+                                        <button className="BuySmallButton">Buy</button>
+                                        <button className="SellSmallButton">Sell</button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
